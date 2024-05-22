59abe0a2d9f795428d4d14b88d57e0742682005c
RobertoCampos <roberto.campos.calvo@est.una.ac.cr>
2024-05-21 23:18:33 -0600

    Observer

---
+++ DesignPatterns/Behavior/Observer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavior
+{
+    public interface Observable
+    {
+        void Attatch(Observer obs);
+        void Deattach(Observer obs);
+        void Notify();
+    }
+    public interface Observer
+    {
+        void Update();
+    }
+
+    public class Follower : Observer
+    {
+        public int Id {  get; set; }
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InstagramProfile : Observable
+    {
+        List<Observer> followers;
+        public void Attatch(Observer follower)
+        {
+            this.followers.Add(follower);
+        }
+
+        public void Deattach(Observer follower)
+        {
+            this.followers.Remove(follower);
+        }
+
+        public void Notify()
+        {
+            foreach (var follower in this.followers)
+            {
+                follower.Update();
+            }
+        }
+
+        public void NewPost()
+        {
+            this.Notify();
+            Console.WriteLine("New post has been uploaded");
+        }
+    }
+}
